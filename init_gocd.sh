762a845316198ea10d6a02293177f95e077d7df8
rlin <rlin@thoughtworks.com>
2017-05-18 15:21:57 +0800

    [@boydfd] storage all data for gocd-server.

--- init_gocd.sh
+++ init_gocd.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 docker-compose down
-rm -rf go
+rm -rf godata
 docker-compose up -d
-#docker exec -it gocd-server chown go:go /etc/go
 docker-compose scale agent=3
-until cat go/cruise-config.xml 2>/dev/null 1>/dev/null
+configFile=godata/config
+until cat ${configFile}/cruise-config.xml 2>/dev/null 1>/dev/null
 do
 	echo 'waiting gocd server generate config file.'
 	sleep 2
 done
 
-sed '/<server/r ./pipelines.xml' ./go/cruise-config.xml > tmp
-mv tmp ./go/cruise-config.xml
+sed '/<server/r ./pipelines.xml' ${configFile}/cruise-config.xml > tmp
+mv tmp ${configFile}/cruise-config.xml
